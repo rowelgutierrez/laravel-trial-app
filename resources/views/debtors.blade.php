d5ce619eca9fde977c9021ce95ab5a1281f6e26c
Rowel Gutierrez <rowel.gutierrez@gmail.com>
2022-03-26 23:17:49 +0800

    modal component for delete alert

--- resources/views/debtors.blade.php
+++ resources/views/debtors.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div x-data="{ open: false }" class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
@@ -29,7 +29,7 @@
                                         <span onclick="window.location='{{ route('creditor.debtor.edit', ['id' => $debtor->id]) }}'">
                                             @svg('edit', ['class' => 'fill-green-600'])
                                         </span>
-                                        <span>
+                                        <span class="openModal" @click="open = ! open">
                                             @svg('delete', ['class' => 'fill-red-600'])
                                         </span>
                                     </div>
@@ -44,5 +44,22 @@
                 {{ __('Add Debtor') }}
             </x-button>
         </div>
+
+        <x-modal ::class="{'block': open, 'hidden': ! open }">
+            <x-slot name="title">
+                {{ __('Delete Debtor') }}
+            </x-slot>
+
+            {{ __('Are you sure you want to delete this debtor?') }}
+
+            <x-slot name="buttons">
+                <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                    {{ __('Delete') }}
+                </button>
+                <button @click="open = ! open" type="button" class="closeModal mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                    {{ __('Cancel') }}
+                </button>
+            </x-slot>
+        </x-modal>
     </div>
 </x-app-layout>
